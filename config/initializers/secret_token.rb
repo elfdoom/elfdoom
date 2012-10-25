ddcbf2ed9147c285404fe521251bb00d83ffb2ae
Elvis Dao <elvis@viralheat.com>
2012-10-24 17:39:40 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elfdoom::Application.config.secret_token = 'e24c3fba94eda96e90452606faea0ea170d6d81c0faa00d038c41625f4dfd400e376cc7be4f186ccbf8dd7a0d97b84353094f17f157eac2f8781716872e9c748'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Elfdoom::Application.config.session_store :cookie_store, :key => '_elfdoom_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Elfdoom::Application.config.session_store :active_record_store
